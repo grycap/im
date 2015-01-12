ff32f05600facc7043cb850722321078678b6131
micafer <micafer1@upv.es>
2015-01-12 12:38:44 +0100

    Improve the Docker connector

---
+++ IM/UnixHTTPConnection.py
@@ -0,0 +1,29 @@
+# IM - Infrastructure Manager
+# Copyright (C) 2011 - GRyCAP - Universitat Politecnica de Valencia
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import httplib
+import socket
+
+class UnixHTTPConnection(httplib.HTTPConnection):
+
+    def __init__(self, path, host='localhost', port=None, strict=None, timeout=None):
+        httplib.HTTPConnection.__init__(self, host, port=port, strict=strict, timeout=timeout)
+        self.path = path
+
+    def connect(self):
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(self.path)
+        self.sock = sock
