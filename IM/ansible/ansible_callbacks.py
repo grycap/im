657e241bb4628f2a2193a2c364c7f9738da117d8
micafer <micafer1@upv.es>
2015-12-16 11:51:14 +0100

    Add support for Ansivble v2.X

--- IM/ansible/ansible_callbacks.py
+++ IM/ansible/ansible_callbacks.py
@@ -1,21 +1,22 @@
-# (C) 2012-2013, Michael DeHaan, <michael.dehaan@gmail.com>
-
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
+# IM - Infrastructure Manager
+# Copyright (C) 2015 - GRyCAP - Universitat Politecnica de Valencia
+# 
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ansible is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more/etc/sudoers details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
-# Miguel: Version simplificada, eliminado el lock "sospechoso"
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Some parts of this code are taken from the Ansible code
+# (c) 2012-2014, Michael DeHaan <michael.dehaan@gmail.com>
+#
 
 import ansible.utils
 import sys
