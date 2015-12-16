4a7969e15bcc919c7803eac1e94dab55cff7f17f
micafer <micafer1@upv.es>
2015-12-16 11:51:14 +0100

    Add support for Ansivble v2.X

--- IM/ansible/ansible_launcher.py
+++ IM/ansible/ansible_launcher.py
@@ -1,34 +1,56 @@
-# (C) 2012, Michael DeHaan, <michael.dehaan@gmail.com>
-
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
+# IM - Infrastructure Manager
+# Copyright (C) 2015 - GRyCAP - Universitat Politecnica de Valencia
+# 
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ansible is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more/etc/sudoers details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Some parts of this code are taken from the Ansible code
+# (c) 2012-2014, Michael DeHaan <michael.dehaan@gmail.com>
+#
 
-#######################################################
-
-# Miguel Caballer: file based on the ansible-playbook
-
+import sys
 import time
 import os
 import threading
-import ansible.playbook
-import ansible.inventory
-import ansible.constants as C
+import logging
 from ansible import errors
-from ansible import utils
+from ansible import __version__ as ansible_version
 
-from ansible_callbacks import display, banner, AggregateStats, PlaybookCallbacks, PlaybookRunnerCallbacks
+if ansible_version.startswith("1."):
+    import ansible.playbook
+    import ansible.inventory
+    import ansible.constants as C
+    from ansible import utils
+    
+    from ansible_callbacks import banner, AggregateStats, PlaybookCallbacks, PlaybookRunnerCallbacks
+else:
+    from ansible.cli import CLI
+    from ansible.parsing.dataloader import DataLoader
+    from ansible.vars import VariableManager
+    from ansible.inventory import Inventory
+    
+    from ansible_executor_v2 import IMPlaybookExecutor
+ 
+
+def display(msg, color=None, stderr=False, screen_only=False, log_only=False, output=None):
+    if output:
+        if isinstance(output, logging.Logger):
+            output.info(msg)
+        else:
+            print >>output, msg
+    else:
+        sys.stdout.write(msg)
+        sys.stdout.flush()
 
 def colorize(lead, num, color):
     """ Print 'lead' = 'num' in 'color' """
@@ -37,127 +59,6 @@ def colorize(lead, num, color):
 def hostcolor(host, stats, color=True):
     return "%-26s" % host
 
-def launch_playbook(output, playbook_file, host, passwd, threads, pk_file = None, retries = 1, inventory_file=None, user=None, extra_vars={}):
-    ''' run ansible-playbook operations '''
-
-    # create parser for CLI options
-    parser = utils.base_parser(
-        constants=C,
-        usage = "%prog playbook.yml",
-        connect_opts=True,
-        runas_opts=True,
-        subset_opts=True,
-        check_opts=True,
-        diff_opts=True
-    )
-
-    options, _ = parser.parse_args([])
-
-    sshpass = None
-    if pk_file:
-        options.private_key_file = pk_file
-    else:
-        sshpass = passwd
-    
-    if user:
-        remote_user=user
-    else:
-        remote_user=options.remote_user
-
-    if not os.path.exists(playbook_file):
-        raise errors.AnsibleError("the playbook: %s could not be found" % playbook_file)
-    if not os.path.isfile(playbook_file):
-        raise errors.AnsibleError("the playbook: %s does not appear to be a file" % playbook_file)
-
-    num_retries = 0
-    return_code = 4
-    hosts_with_errors = []
-    while return_code != 0 and num_retries < retries:
-        time.sleep(5*num_retries)
-        num_retries += 1
-        return_code = 0
-
-        if inventory_file:
-            inventory = ansible.inventory.Inventory(inventory_file)
-        else:
-            inventory = ansible.inventory.Inventory(options.inventory)
-            
-        inventory.subset(host)
-        # let inventory know which playbooks are using so it can know the basedirs
-        inventory.set_playbook_basedir(os.path.dirname(playbook_file))
-
-        stats = AggregateStats()
-        playbook_cb = PlaybookCallbacks(verbose=utils.VERBOSITY, output=output)
-        runner_cb = PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY, output=output)
-
-        pb = ansible.playbook.PlayBook(
-            playbook=playbook_file,
-            module_path=options.module_path,
-            inventory=inventory,
-            forks=threads,
-            remote_user=remote_user,
-            remote_pass=sshpass,
-            callbacks=playbook_cb,
-            runner_callbacks=runner_cb,
-            stats=stats,
-            extra_vars=extra_vars,
-            private_key_file=options.private_key_file,
-            only_tags=['all']
-        )
-
-        try:
-            failed_hosts = []
-            unreachable_hosts = []
-
-            pb.run()
-
-            hosts = sorted(pb.stats.processed.keys())
-            display(banner("PLAY RECAP"), output=output)
-            playbook_cb.on_stats(pb.stats)
-
-            for h in hosts:
-                t = pb.stats.summarize(h)
-                if t['failures'] > 0:
-                    failed_hosts.append(h)
-                if t['unreachable'] > 0:
-                    unreachable_hosts.append(h)
-
-            hosts_with_errors = failed_hosts + unreachable_hosts
-
-            for h in hosts:
-                t = pb.stats.summarize(h)
-
-                display("%s : %s %s %s %s" % (
-                    hostcolor(h, t),
-                    colorize('ok', t['ok'], 'green'),
-                    colorize('changed', t['changed'], 'yellow'),
-                    colorize('unreachable', t['unreachable'], 'red'),
-                    colorize('failed', t['failures'], 'red')),
-                    screen_only=True, output=output
-                )
-
-                display("%s : %s %s %s %s" % (
-                    hostcolor(h, t, False),
-                    colorize('ok', t['ok'], None),
-                    colorize('changed', t['changed'], None),
-                    colorize('unreachable', t['unreachable'], None),
-                    colorize('failed', t['failures'], None)),
-                    log_only=True, output=output
-                )
-
-            if len(failed_hosts) > 0:
-                return_code = 2
-            if len(unreachable_hosts) > 0:
-                return_code = 3
-
-        except errors.AnsibleError, e:
-            display("ERROR: %s" % e, color='red', output=output)
-            return_code = 1
-        
-        if return_code != 0:
-            display("ERROR executing playbook (%s/%s)" % (num_retries, retries), color='red', output=output)
-
-    return (return_code, hosts_with_errors)
 
 
 class AnsibleThread(threading.Thread):
@@ -181,7 +82,213 @@ class AnsibleThread(threading.Thread):
         
     def run(self):
         try:
-            self.results = launch_playbook(self.output, self.playbook_file, self.host, self.passwd, self.threads, self.pk_file, self.retries, self.inventory_file, self.user, self.extra_vars)
+            if ansible_version.startswith("1."):
+                self.results = self.launch_playbook_v1()
+            else:
+                self.results = self.launch_playbook_v2()
         except errors.AnsibleError, e:
-            display("ERROR: %s" % e, color='red', stderr=True, output=self.output)
+            display("ERROR: %s" % e, output=self.output)
             self.results = (1, [])
+
+    def launch_playbook_v2(self):
+        ''' run ansible-playbook operations v2.X'''
+        # create parser for CLI options
+        parser = CLI.base_parser(
+            usage = "%prog playbook.yml",
+            connect_opts=True,
+            meta_opts=True,
+            runas_opts=True,
+            subset_opts=True,
+            check_opts=True,
+            inventory_opts=True,
+            runtask_opts=True,
+            vault_opts=True,
+            fork_opts=True,
+            module_opts=True,
+        )
+    
+        options, _ = parser.parse_args([])
+    
+        sshpass    = None
+    
+        if self.pk_file:
+            options.private_key_file = self.pk_file
+        else:
+            sshpass = self.passwd
+        
+        passwords = { 'conn_pass': sshpass, 'become_pass': sshpass }
+    
+        if self.user:
+            options.remote_user=self.user
+    
+        if not os.path.exists(self.playbook_file):
+            raise errors.AnsibleError("the playbook: %s could not be found" % self.playbook_file)
+        if not os.path.isfile(self.playbook_file):
+            raise errors.AnsibleError("the playbook: %s does not appear to be a file" % self.playbook_file)
+
+        variable_manager = VariableManager()
+        variable_manager.extra_vars = self.extra_vars
+        
+        if self.inventory_file:
+            options.inventory = self.inventory_file 
+    
+        options.forks = self.threads
+    
+        loader = DataLoader()
+        inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=options.inventory)
+        variable_manager.set_inventory(inventory)
+        
+        inventory.subset(self.host)
+        # let inventory know which playbooks are using so it can know the basedirs
+        inventory.set_playbook_basedir(os.path.dirname(self.playbook_file))
+    
+        num_retries = 0
+        return_code = 4
+        results = None
+    
+        while return_code != 0 and num_retries < self.retries:
+            time.sleep(5*num_retries)
+            num_retries += 1
+            return_code = 0
+    
+            try:
+                # create the playbook executor, which manages running the plays via a task queue manager
+                pbex = IMPlaybookExecutor(playbooks=[self.playbook_file], 
+                                    inventory=inventory, 
+                                    variable_manager=variable_manager, 
+                                    loader=loader, 
+                                    options=options, 
+                                    passwords=passwords,
+                                    output=self.output)
+        
+                return_code = pbex.run()
+                    
+            except errors.AnsibleError, e:
+                display("ERROR: %s" % e, output=self.output)
+                return_code = 1
+            
+            if return_code != 0:
+                display("ERROR executing playbook (%s/%s)" % (num_retries, self.retries),  output=self.output)
+    
+        return (return_code, results)
+    
+    def launch_playbook_v1(self):
+        ''' run ansible-playbook operations v1.X'''
+        # create parser for CLI options
+        parser = utils.base_parser(
+            constants=C,
+            usage = "%prog playbook.yml",
+            connect_opts=True,
+            runas_opts=True,
+            subset_opts=True,
+            check_opts=True,
+            diff_opts=True
+        )
+    
+        options, _ = parser.parse_args([])
+    
+        sshpass = None
+        if self.pk_file:
+            options.private_key_file = self.pk_file
+        else:
+            sshpass = self.passwd
+        
+        if self.user:
+            remote_user=self.user
+        else:
+            remote_user=options.remote_user
+    
+        if not os.path.exists(self.playbook_file):
+            raise errors.AnsibleError("the playbook: %s could not be found" % self.playbook_file)
+        if not os.path.isfile(self.playbook_file):
+            raise errors.AnsibleError("the playbook: %s does not appear to be a file" % self.playbook_file)
+    
+        num_retries = 0
+        return_code = 4
+        hosts_with_errors = []
+        while return_code != 0 and num_retries < self.retries:
+            time.sleep(5*num_retries)
+            num_retries += 1
+            return_code = 0
+    
+            if self.inventory_file:
+                inventory = ansible.inventory.Inventory(self.inventory_file)
+            else:
+                inventory = ansible.inventory.Inventory(options.inventory)
+                
+            inventory.subset(self.host)
+            # let inventory know which playbooks are using so it can know the basedirs
+            inventory.set_playbook_basedir(os.path.dirname(self.playbook_file))
+    
+            stats = AggregateStats()
+            playbook_cb = PlaybookCallbacks(verbose=utils.VERBOSITY, output=self.output)
+            runner_cb = PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY, output=self.output)
+    
+            pb = ansible.playbook.PlayBook(
+                playbook=self.playbook_file,
+                module_path=options.module_path,
+                inventory=inventory,
+                forks=self.threads,
+                remote_user=remote_user,
+                remote_pass=sshpass,
+                callbacks=playbook_cb,
+                runner_callbacks=runner_cb,
+                stats=stats,
+                extra_vars=self.extra_vars,
+                private_key_file=options.private_key_file,
+                only_tags=['all']
+            )
+    
+            try:
+                failed_hosts = []
+                unreachable_hosts = []
+    
+                pb.run()
+    
+                hosts = sorted(pb.stats.processed.keys())
+                display(banner("PLAY RECAP"), output=self.output)
+                playbook_cb.on_stats(pb.stats)
+    
+                for h in hosts:
+                    t = pb.stats.summarize(h)
+                    if t['failures'] > 0:
+                        failed_hosts.append(h)
+                    if t['unreachable'] > 0:
+                        unreachable_hosts.append(h)
+    
+                hosts_with_errors = failed_hosts + unreachable_hosts
+    
+                for h in hosts:
+                    t = pb.stats.summarize(h)
+    
+                    display("%s : %s %s %s %s" % (
+                        hostcolor(h, t),
+                        colorize('ok', t['ok'], 'green'),
+                        colorize('changed', t['changed'], 'yellow'),
+                        colorize('unreachable', t['unreachable'], 'red'),
+                        colorize('failed', t['failures'], 'red')),
+                        screen_only=True, output=self.output
+                    )
+    
+                    display("%s : %s %s %s %s" % (
+                        hostcolor(h, t, False),
+                        colorize('ok', t['ok'], None),
+                        colorize('changed', t['changed'], None),
+                        colorize('unreachable', t['unreachable'], None),
+                        colorize('failed', t['failures'], None)),
+                        log_only=True, output=self.output
+                    )
+    
+                if len(failed_hosts) > 0:
+                    return_code = 2
+                if len(unreachable_hosts) > 0:
+                    return_code = 3
+    
+            except errors.AnsibleError, e:
+                display("ERROR: %s" % e, color='red', output=self.output)
+                return_code = 1
+            
+            if return_code != 0:
+                display("ERROR executing playbook (%s/%s)" % (num_retries, self.retries), color='red', output=self.output)
+    
+        return (return_code, hosts_with_errors)
