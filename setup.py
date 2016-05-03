9d671805007043dbd877f5add8fee2f79d825bd7
micafer <micafer1@upv.es>
2016-05-03 10:01:44 +0200

    Remove the restriction to use Ansible v2.X in setup

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(name="IM", version=version,
                         "the user with a fully functional infrastructure."),
       description="IM is a tool to manage virtual infrastructures on Cloud deployments",
       platforms=["any"],
-      install_requires=["ansible >= 1.8, < 2", "paramiko >= 1.14", "PyYAML", "SOAPpy",
+      install_requires=["ansible >= 1.8", "paramiko >= 1.14", "PyYAML", "SOAPpy",
                         "boto >= 2.29", "apache-libcloud >= 0.17", "RADL", "bottle", "netaddr", "scp"]
       )
