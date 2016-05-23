9e580e7ed84ea88a5185eb59e6e8788c089b5a98
micafer <micafer1@upv.es>
2016-05-23 17:28:55 +0200

    Add DATA_DB env to config

--- IM/config.py
+++ IM/config.py
@@ -99,7 +99,7 @@ section_name = "im"
 if config.has_section(section_name):
     parse_options(config, section_name, Config)
 
-# Get some vars from env variables to make easy docker container configuration
+# Get some vars from environment variables to make easy docker container configuration
 if 'DATA_DB' in os.environ:
     Config.DATA_DB = os.environ['DATA_DB']
 
