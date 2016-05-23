86b1c432ed6584202faaf46ecd53da21035a0446
micafer <micafer1@upv.es>
2016-05-23 17:08:24 +0200

    Add DATA_DB env to config

--- IM/config.py
+++ IM/config.py
@@ -99,6 +99,9 @@ section_name = "im"
 if config.has_section(section_name):
     parse_options(config, section_name, Config)
 
+# Get some vars from env variables to make easy docker container configuration
+if 'DATA_DB' in os.environ:
+    Config.DATA_DB = os.environ['DATA_DB']
 
 class ConfigOpenNebula:
     TEMPLATE_CONTEXT = ''
