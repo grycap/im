d95d7a3c42ea39fdc7708c2823bd891b5dd1c4d8
micafer <micafer1@upv.es>
2016-07-06 08:58:30 +0200

    Remove the ansible requirement as it makes to generate an incorrect dependency python-ansible

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -2,6 +2,8 @@
 
 apt update
 apt install -y python-stdeb
+# remove the ansible requirement as it makes to generate an incorrect dependency python-ansible
+sed -i '/install_requires/c\      install_requires=["paramiko >= 1.14", "PyYAML", "SOAPpy",' setup.py
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-tosca-parser, python-radl, ansible, python-paramiko, python-yaml, python-soappy, python-boto, python-libcloud, python-bottle, python-netaddr, python-scp" bdist_deb
 mkdir dist_pkg
 cp deb_dist/*.deb dist_pkg
