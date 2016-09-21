5ab540b47932c27b73418fcf5ac7ad3c9990a841
micafer <micafer1@upv.es>
2016-09-21 17:26:25 +0200

    Join again wait_master and check_vm_ips threads

--- IM/InfrastructureInfo.py
+++ IM/InfrastructureInfo.py
@@ -436,9 +436,8 @@ class InfrastructureInfo:
                 max_ctxt_time = Config.MAX_CONTEXTUALIZATION_TIME
 
             ctxt_task = []
-            ctxt_task.append((-4, 0, self, ['kill_ctxt_processes']))
-            ctxt_task.append((-3, 0, self, ['wait_master']))
-            ctxt_task.append((-2, 0, self, ['check_vm_ips']))
+            ctxt_task.append((-3, 0, self, ['kill_ctxt_processes']))
+            ctxt_task.append((-2, 0, self, ['wait_master', 'check_vm_ips']))
             ctxt_task.append(
                 (-1, 0, self, ['configure_master', 'generate_playbooks_and_hosts']))
 
