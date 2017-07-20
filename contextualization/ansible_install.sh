10c5a49b2ee5fb69f9ac5af290667ad2cfaad5b6
micafer <micafer1@upv.es>
2017-07-20 12:04:35 +0200

    Improve install ansible script

--- contextualization/ansible_install.sh
+++ contextualization/ansible_install.sh
@@ -1,109 +1,92 @@
 #!/bin/sh
-#TODO.md
-yum_makecache_retry() {
-  tries=0
-  until [ $tries -ge 5 ]
-  do
-    yum makecache && break
-    let tries++
-    sleep 1
-  done
+
+distribution_id() {
+    RETVAL=""
+    if [ -z "${RETVAL}" -a -e "/etc/os-release" ]; then
+        . /etc/os-release
+        RETVAL="${ID}"
+    fi
+
+    if [ -z "${RETVAL}" -a -e "/etc/centos-release" ]; then
+        RETVAL="centos"
+    fi
+
+    if [ -z "${RETVAL}" -a -e "/etc/fedora-release" ]; then
+        RETVAL="fedora"
+    fi
+
+    if [ -z "${RETVAL}" -a -e "/etc/redhat-release" ]; then
+        RELEASE_OUT=$(head -n1 /etc/redhat-release)
+        case "${RELEASE_OUT}" in
+            Red\ Hat\ Enterprise\ Linux*)
+                RETVAL="rhel"
+                ;;
+            CentOS*)
+                RETVAL="centos"
+                ;;
+            Fedora*)
+                RETVAL="fedora"
+                ;;
+        esac
+    fi
+
+    if [ -z "${RETVAL}" ]; then
+        RETVAL="unknown"
+    fi
+
+    echo ${RETVAL}
 }
 
-if [ "x$KITCHEN_LOG" = "xDEBUG" -o "x$OMNIBUS_ANSIBLE_LOG" = "xDEBUG" ]; then
-  export PS4='(${BASH_SOURCE}:${LINENO}): - [${SHLVL},${BASH_SUBSHELL},$?] $ '
-  set -x
-fi
+distribution_major_version() {
+    for RELEASE_FILE in /etc/system-release \
+                        /etc/centos-release \
+                        /etc/fedora-release \
+                        /etc/redhat-release
+    do
+        if [ -e "${RELEASE_FILE}" ]; then
+            RELEASE_VERSION=$(head -n1 ${RELEASE_FILE})
+            break
+        fi
+    done
+    echo ${RELEASE_VERSION} | sed -e 's|\(.\+\) release \([0-9]\+\)\([0-9.]*\).*|\2|'
+}
 
-if [ ! $(which ansible-playbook) ]; then
-  if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ] || [ -f /etc/oracle-release ] || [ -f /etc/system-release ] || grep -q 'Amazon Linux' /etc/system-release; then
+ansible_installed=`type -p ansible-playbook`
 
-    # Install required Python libs and pip
-    # Fix EPEL Metalink SSL error
-    # - workaround: https://community.hpcloud.com/article/centos-63-instance-giving-cannot-retrieve-metalink-repository-epel-error
-    # - SSL secure solution: Update ca-certs!!
-    #   - http://stackoverflow.com/q/26734777/645491#27667111
-    #   - http://serverfault.com/q/637549/77156
-    #   - http://unix.stackexchange.com/a/163368/7688
-    yum -y install ca-certificates nss
-    yum clean all
-    rm -rf /var/cache/yum
-    yum_makecache_retry
-    yum -y install epel-release
-    # One more time with EPEL to avoid failures
-    yum_makecache_retry
-
-    yum -y install python-pip PyYAML python-jinja2 python-httplib2 python-keyczar python-paramiko git
-    # If python-pip install failed and setuptools exists, try that
-    if [ -z "$(which pip)" -a -z "$(which easy_install)" ]; then
-      yum -y install python-setuptools
-      easy_install pip
-    elif [ -z "$(which pip)" -a -n "$(which easy_install)" ]; then
-      easy_install pip
-    fi
-
-    # Install passlib for encrypt
-    yum -y groupinstall "Development tools"
-    yum -y install python-devel MySQL-python sshpass && pip install pyrax pysphere boto passlib dnspython
-
-    # Install Ansible module dependencies
-    yum -y install bzip2 file findutils git gzip hg svn sudo tar which unzip xz zip libselinux-python
-    [ -n "$(yum search procps-ng)" ] && yum -y install procps-ng || yum -y install procps
-  elif [ -f /etc/debian_version ] || [ grep -qi ubuntu /etc/lsb-release ] || grep -qi ubuntu /etc/os-release; then
-    apt-get update
-    # Install via package
-    # apt-get update && \
-    # apt-get install --no-install-recommends -y software-properties-common && \
-    # apt-add-repository ppa:ansible/ansible && \
-    # apt-get update && \
-    # apt-get install -y ansible
-
-    # Install required Python libs and pip
-    apt-get install -y  python-pip python-yaml python-jinja2 python-httplib2 python-paramiko python-pkg-resources
-    [ -n "$( apt-cache search python-keyczar )" ] && apt-get install -y  python-keyczar
-    if ! apt-get install -y git ; then
-      apt-get install -y git-core
-    fi
-    # If python-pip install failed and setuptools exists, try that
-    if [ -z "$(which pip)" -a -z "$(which easy_install)" ]; then
-      apt-get -y install python-setuptools
-      easy_install pip
-    elif [ -z "$(which pip)" -a -n "$(which easy_install)" ]; then
-      easy_install pip
-    fi
-    # If python-keyczar apt package does not exist, use pip
-    [ -z "$( apt-cache search python-keyczar )" ] && sudo pip install python-keyczar
-
-    # Install passlib for encrypt
-    apt-get install -y build-essential
-    apt-get install -y python-all-dev python-mysqldb sshpass && pip install pyrax pysphere boto passlib dnspython
-
-    # Install Ansible module dependencies
-    apt-get install -y bzip2 file findutils git gzip mercurial procps subversion sudo tar debianutils unzip xz-utils zip python-selinux
-
-  else
-    echo 'WARN: Could not detect distro or distro unsupported'
-    echo 'WARN: Trying to install ansible via pip without some dependencies'
-    echo 'WARN: Not all functionality of ansible may be available'
-  fi
-
-  mkdir /etc/ansible/
-  echo -e '[local]\nlocalhost\n' > /etc/ansible/hosts
-  pip install ansible
-
-  if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ] || [ -f /etc/oracle-release ] || [ -f /etc/system-release ] || grep -q 'Amazon Linux' /etc/system-release; then
-    # Fix for pycrypto pip / yum issue
-    # https://github.com/ansible/ansible/issues/276
-    if  ansible --version 2>&1  | grep -q "AttributeError: 'module' object has no attribute 'HAVE_DECL_MPZ_POWM_SEC'" ; then
-      echo 'WARN: Re-installing python-crypto package to workaround ansible/ansible#276'
-      echo 'WARN: https://github.com/ansible/ansible/issues/276'
-      pip uninstall -y pycrypto
-      yum erase -y python-crypto
-      yum install -y python-crypto python-paramiko
-    fi
-  fi  
+if [ x${ansible_installed}x != "xx" ]
+then
+    echo "Ansible installed. Do not install."
 else
-  echo "Ansible already installed"
+    echo "Ansible not installed. Installing ..."
+    DISTRO=$(distribution_id)
+    case $DISTRO in
+        debian)
+            echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+            apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+            apt-get update
+            apt-get -y install wget ansible
+            ;;
+        ubuntu)
+            apt-get -y install software-properties-common
+            apt-add-repository -y ppa:ansible/ansible
+            apt-get update
+            apt-get -y install wget ansible
+            ;;
+        rhel)
+            yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-$(distribution_major_version).noarch.rpm
+            yum install -y wget ansible
+            ;;
+        centos)
+            yum install -y epel-release wget
+            yum install -y ansible
+            ;;
+        fedora)
+            yum install -y wget ansible python2-rpm yum
+            ;;
+    	*)
+            echo "Unsupported distribution: $DISTRO"
+            ;;
+    esac
 fi
 
 # Create the config file
@@ -119,9 +102,6 @@ record_host_keys=False
 [ssh_connection]
 pipelining = True
 EOL
-
-cp -rf /tmp/ansible_roles /etc/ansible/roles
-rm -rf /tmp/roles
 
 if [ $(which ansible-playbook) ]; then
 	echo '{"OK" : true}' > $1
