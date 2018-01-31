bf96ed57b5e0c387357774a6392d5f4655fa4d4c
micafer <micafer1@upv.es>
2018-01-31 11:43:09 +0100

    Fix test

--- test/unit/Tosca.py
+++ test/unit/Tosca.py
@@ -59,7 +59,10 @@ class TestTosca(unittest.TestCase):
         else:
             self.assertEqual(net.getValue('provider_id'), 'vpc-XX.subnet-XX')
             self.assertEqual(net.getValue("outports"), '8080/tcp-8080/tcp')
-            self.assertEqual(net1.getValue("outports"), '1:4/tcp')
+            self.assertIn('1:4/tcp', net1.getValue("outports"))
+            self.assertIn('80/tcp-80/tcp', net1.getValue("outports"))
+            self.assertIn('8080/tcp-8080/tcp', net1.getValue("outports"))
+            
         lrms_wn = radl.get_system_by_name('lrms_wn')
         self.assertEqual(lrms_wn.getValue('memory.size'), 2000000000)
         lrms_server = radl.get_system_by_name('lrms_server')
