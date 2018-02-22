981fbf94b224a091c09d3b71c07af8381165d0bf
micafer <micafer1@upv.es>
2018-02-22 15:45:37 +0100

    Increase timeout

--- test/integration/TestREST.py
+++ test/integration/TestREST.py
@@ -447,7 +447,7 @@ class TestIM(unittest.TestCase):
         vm_ids = resp.text.split("\n")
         self.assertEqual(len(vm_ids), 3, msg=("ERROR getting infrastructure info: Incorrect number of VMs(" +
                                               str(len(vm_ids)) + "). It must be 2"))
-        all_configured = self.wait_inf_state(VirtualMachine.CONFIGURED, 600)
+        all_configured = self.wait_inf_state(VirtualMachine.CONFIGURED, 1200)
         self.assertTrue(
             all_configured, msg="ERROR waiting the infrastructure to be configured (timeout).")
 
@@ -468,7 +468,7 @@ class TestIM(unittest.TestCase):
         vm_ids = resp.text.split("\n")
         self.assertEqual(len(vm_ids), 2, msg=("ERROR getting infrastructure info: Incorrect number of VMs(" +
                                               str(len(vm_ids)) + "). It must be 2"))
-        all_configured = self.wait_inf_state(VirtualMachine.CONFIGURED, 600)
+        all_configured = self.wait_inf_state(VirtualMachine.CONFIGURED, 900)
         self.assertTrue(
             all_configured, msg="ERROR waiting the infrastructure to be configured (timeout).")
 
