ccbed8afae0c1dbceae29e40e298b7cb06322d5d
micafer <micafer1@upv.es>
2018-04-11 12:15:27 +0200

    Fix py3 test issue

--- test/unit/Tosca.py
+++ test/unit/Tosca.py
@@ -57,7 +57,8 @@ class TestTosca(unittest.TestCase):
         else:
             self.assertEqual(net.getValue('provider_id'), 'vpc-XX.subnet-XX')
             self.assertEqual(net.getValue("outports"), '8080/tcp-8080/tcp')
-            self.assertEqual(net1.getValue("outports"), '1:4/tcp,8000/tcp-8000/tcp')
+            self.assertIn('1:4/tcp', net1.getValue("outports"))
+            self.assertIn('8000/tcp-8000/tcp', net1.getValue("outports"))
         lrms_wn = radl.get_system_by_name('lrms_wn')
         self.assertEqual(lrms_wn.getValue('memory.size'), 2000000000)
         lrms_server = radl.get_system_by_name('lrms_server')
