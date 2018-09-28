decf40f0eccb444a51f3e60dfdc6526e7bf0d79a
micafer <micafer1@upv.es>
2018-09-28 12:59:08 +0200

    Fix #678

--- contextualization/ctxt_agent.py
+++ contextualization/ctxt_agent.py
@@ -210,7 +210,7 @@ class CtxtAgent():
         thread, result = thread_data
         thread.join()
         try:
-            _, (return_code, hosts_with_errors), _ = result.get(timeout=60)
+            _, (return_code, hosts_with_errors), _ = result.get(timeout=60, block=False)
         except:
             CtxtAgent.logger.exception('Error getting ansible results.')
             return_code = -1
