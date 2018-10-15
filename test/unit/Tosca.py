30c78dcb9bc411314173cb357219e447e9244eb7
micafer <micafer1@upv.es>
2018-10-15 12:39:59 +0200

    Implements #687

--- test/unit/Tosca.py
+++ test/unit/Tosca.py
@@ -66,6 +66,8 @@ class TestTosca(unittest.TestCase):
             self.assertIn('80/tcp-80/tcp', net1.getValue("outports"))
             self.assertIn('8080/tcp-8080/tcp', net1.getValue("outports"))
 
+        self.assertIn('10000/tcp-10000/tcp', net2.getValue("outports"))
+
         lrms_wn = radl.get_system_by_name('lrms_wn')
         self.assertEqual(lrms_wn.getValue('memory.size'), 2000000000)
         lrms_server = radl.get_system_by_name('lrms_server')
