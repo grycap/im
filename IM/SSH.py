a025f849ae27422469b1ff3a5c31338ae5270777
micafer <micafer1@upv.es>
2018-10-24 08:32:56 +0200

    Chage comment

--- IM/SSH.py
+++ IM/SSH.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-""" Classes to encapsulate SSH operations using paramiko """
+""" Classes to encapsulate SSH operations using ssh2-python lib """
 
 import socket
 from ssh2.session import Session
