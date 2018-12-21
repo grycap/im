7af049eb5368515f201b5134449403287899ccac
micafer <micafer1@upv.es>
2018-12-21 08:23:31 +0100

    Improve monitoring probe

--- monitoring/probeim.py
+++ monitoring/probeim.py
@@ -1,12 +1,185 @@
+#! /usr/bin/env python
 import time
 import os
 import argparse
 import sys
-
+import json
+import requests
 import logging
 from logging.handlers import RotatingFileHandler
 
-import IMinfrastructureOper
+try:
+    # To avoid annoying InsecureRequestWarning messages in some Connectors
+    import requests.packages
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+except:
+    pass
+
+
+def read_file_as_string(file_name):
+    tests_path = os.path.dirname(os.path.abspath(__file__))
+    abs_file_path = os.path.join(tests_path, file_name)
+    return open(abs_file_path, 'r').read()
+
+
+class ResponseIM:
+
+    def __init__(self, statuscode, info):
+        self.statuscode = statuscode
+        self.info = info
+
+
+class IM:
+
+    @staticmethod
+    def update_imheaders(tokenstring=None):
+
+        vlines = read_file_as_string('./conf/authorizationHeader.txt')
+
+        authoriz = ''
+        i = 0
+
+        for line in vlines.split("\n"):
+            i += 1
+            line = line.rstrip()
+            if tokenstring:
+                authoriz += line + " token = " + tokenstring
+            else:
+                authoriz += line + " username = mon_user; password = mon_test_1X"
+
+            if len(vlines) > i:
+                authoriz += "\\n  "
+
+        authoriz += " ;"
+
+        UPD_HEADERS = {
+            "Content-Type": "text/plain",
+            "Accept": "application/json",
+            "Authorization": authoriz
+        }
+
+        return UPD_HEADERS
+
+    @staticmethod
+    def requestIM(method, url, data, headers, verify):
+        try:
+            r = requests.request(method, url, data=data, headers=headers, verify=verify)
+            rq = ResponseIM(r.status_code, r.text)
+        except requests.ConnectionError as e:
+            logging.error("* ConnectionError exception at method " + method + ": " + str(e))
+            rq = ResponseIM(111, e)
+        except requests.exceptions.InvalidHeader as e:
+            logging.error("* InvalidHeader exception: ---> " + str(e) + " <--")
+            rq = ResponseIM(111, e)
+
+        return rq
+
+    @staticmethod
+    def list_infrastructure(url, imheaders):
+
+        r = IM.requestIM('GET', url + "/infrastructures", {}, imheaders, False)
+
+        if r.statuscode == 200:
+            ret = ResponseIM(r.statuscode, 'list method OK')
+        elif r.statuscode == 111:
+            ret = ResponseIM(r.statuscode, r.info)
+        else:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['message'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error("list_infrastructure" + str(e))
+
+        return ret
+
+    @staticmethod
+    def create_infrastructure(url, imheaders):
+        # hold on a little bit for the IM to get ready
+        time.sleep(3)
+        radl = read_file_as_string('./conf/test.radl')
+        r = IM.requestIM('POST', url + "/infrastructures", radl, imheaders, False)
+
+        if r.statuscode == 200:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['uri'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error(e)
+
+        elif r.statuscode == 111:
+            ret = ResponseIM(r.statuscode, r.info)
+        else:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['message'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error("create_infrastructure" + str(e))
+        return ret
+
+    @staticmethod
+    def start_infrastructure(imheaders, uri_inf_id):
+
+        time.sleep(3)
+        r = IM.requestIM('PUT', uri_inf_id + '/start', {}, imheaders, False)
+
+        if r.statuscode == 200:
+            ret = ResponseIM(r.statuscode, 'start method OK')
+        elif r.statuscode == 111:
+            ret = ResponseIM(r.statuscode, r.info)
+        else:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['message'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error("start_infrastructure" + str(e))
+
+        return ret
+
+    @staticmethod
+    def create_vm(IMHEADERS, uri_inf_id):
+
+        time.sleep(3)
+        radl = read_file_as_string('./conf/test.radl')
+        r = IM.requestIM('POST', uri_inf_id, radl, IMHEADERS, False)
+
+        if r.statuscode == 200:
+            ret = ResponseIM(r.statuscode, 'Creation of VM is OK')
+        elif r.statuscode == 111:
+            ret = ResponseIM(r.statuscode, r.info)
+        else:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['message'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error("create_vm" + str(e))
+
+        return ret
+
+    @staticmethod
+    def delete_infrastructure(imheaders, uri_inf_id):
+
+        time.sleep(3)
+        r = IM.requestIM('DELETE', uri_inf_id, {}, imheaders, False)
+
+        if r.statuscode == 200:
+            ret = ResponseIM(r.statuscode, 'delete_infrastructure is OK')
+        elif r.statuscode == 111:
+            ret = ResponseIM(r.statuscode, r.info)
+        else:
+            try:
+                json_data = json.loads(r.info)
+                ret = ResponseIM(r.statuscode, json_data['message'])
+            except ValueError as e:
+                ret = ResponseIM(111, e)
+                logging.error("delete_infrastructure" + str(e))
+
+        return ret
 
 
 def log_setup(loglevel):
@@ -36,12 +209,10 @@ def log_setup(loglevel):
 
 def main(url, token):
 
-    if token == "None":
-        token = None
-    imheaders = IMinfrastructureOper.update_imheaders(token)
+    imheaders = IM.update_imheaders(token)
 
     # CREATE INFRASTRUCTURE
-    ci = IMinfrastructureOper.create_infrastructure(url, imheaders)
+    ci = IM.create_infrastructure(url, imheaders)
     url_infr = ci.info
 
     if ci.statuscode == 401:
@@ -53,21 +224,21 @@ def main(url, token):
 
     elif ci.statuscode == 200:
         # START INFRASTRUCTUREWARNING
-        si = IMinfrastructureOper.start_infrastructure(imheaders, url_infr)
+        si = IM.start_infrastructure(imheaders, url_infr)
 
         if si.statuscode == 200:
 
             time.sleep(1)
             # LIST INFRASTRUCTURE
-            li = IMinfrastructureOper.list_infrastructure(url, imheaders)
+            li = IM.list_infrastructure(url, imheaders)
 
             if li.statuscode == 200:
                 # CREATE VM
-                cv = IMinfrastructureOper.create_vm(imheaders, url_infr)
+                cv = IM.create_vm(imheaders, url_infr)
 
                 if cv.statuscode == 200:
                     # DELETE INFRASTRUCTURE
-                    di = IMinfrastructureOper.delete_infrastructure(imheaders, url_infr)
+                    di = IM.delete_infrastructure(imheaders, url_infr)
 
                     if di.statuscode == 200:
                         logging.info("All operations have been completed successfully.")
@@ -100,11 +271,10 @@ if __name__ == '__main__':
 
     # Parse input arguments
     parser = argparse.ArgumentParser(description='Monitorize IM operations.')
-    parser.add_argument('-t', '--token', help='STRING of access token')
     parser.add_argument('-u', '--url', help='URL of the IM REST API endpoint', default="http://localhost:8800")
+    parser.add_argument('-t', '--token', help='STRING of access token', default=None)
     args = parser.parse_args()
 
-    # get client credential and token
     logging.info("Initializing --------------")
 
     succcess, msg = main(args.url, args.token)
@@ -113,4 +283,4 @@ if __name__ == '__main__':
         sys.exit(0)
     else:
         print(msg)
-        sys.exit(1)
+        sys.exit(2)
--- test/unit/test_probeim.py
+++ test/unit/test_probeim.py
@@ -7,7 +7,7 @@ sys.path.append(".")
 sys.path.append("../monitoring")
 sys.path.append("./monitoring")
 
-import IMinfrastructureOper
+import probeim
 
 
 class MockResponse:
@@ -19,57 +19,57 @@ class MockResponse:
 
 class TestProbeIMZabbix(unittest.TestCase):
 
-    @mock.patch('IMinfrastructureOper.requestIM')
+    @mock.patch('probeim.IM.requestIM')
     @mock.patch('time.sleep')
     def test_list_infrastructure(self, _, request_get_mock):
         """ Testing IMinfrastructureOper.list_infrastructure """
         request_get_mock.return_value = MockResponse({"key2": "value2"}, 200)
         anyheader = {"headerkey": "headervalue"}
-        rl = IMinfrastructureOper.list_infrastructure("http://server.com:8800", anyheader)
+        rl = probeim.IM.list_infrastructure("http://server.com:8800", anyheader)
         assert rl.statuscode == 200
         assert rl.info == 'list method OK'
 
-    @mock.patch('IMinfrastructureOper.requestIM')
+    @mock.patch('probeim.IM.requestIM')
     @mock.patch('time.sleep')
     def test_create_infrastructure(self, _, request_post_mock):
         """ Testing IMinfrastructureOper.create_infrastructure """
         request_post_mock.return_value = MockResponse('{"uri": "any/url"}', 200)
         anyheader = {"headerkey": "headervalue"}
-        rc = IMinfrastructureOper.create_infrastructure("http://server.com:8800", anyheader)
+        rc = probeim.IM.create_infrastructure("http://server.com:8800", anyheader)
         jsonuri = json.loads(str(request_post_mock.return_value.info))
         assert rc.statuscode == 200
         assert str(jsonuri['uri']) == 'any/url'
 
-    @mock.patch('IMinfrastructureOper.requestIM')
+    @mock.patch('probeim.IM.requestIM')
     @mock.patch('time.sleep')
     def test_start_infrastructure(self, _, request_put_mock):
         """ Testing IMinfrastructureOper.start_infrastructure """
         request_put_mock.return_value = MockResponse({}, 200)
         anyheader = {"headerkey": "headervalue"}
         anyurl = 'any/url'
-        rc = IMinfrastructureOper.start_infrastructure(anyheader, anyurl)
+        rc = probeim.IM.start_infrastructure(anyheader, anyurl)
         assert rc.statuscode == 200
         assert rc.info == 'start method OK'
 
-    @mock.patch('IMinfrastructureOper.requestIM')
+    @mock.patch('probeim.IM.requestIM')
     @mock.patch('time.sleep')
     def test_create_vm(self, _, request_vmpost_mock):
         """ Testing IMinfrastructureOper.create_vm """
         request_vmpost_mock.return_value = MockResponse({"uri-list": []}, 200)
         anyheader = {"headerkey": "headervalue"}
         anyurl = 'any/url'
-        cv = IMinfrastructureOper.create_vm(anyheader, anyurl)
+        cv = probeim.IM.create_vm(anyheader, anyurl)
         assert cv.statuscode == 200
         assert cv.info == 'Creation of VM is OK'
 
-    @mock.patch('IMinfrastructureOper.requestIM')
+    @mock.patch('probeim.IM.requestIM')
     @mock.patch('time.sleep')
     def test_delete_vm(self, _, request_delete_mock):
         """ Testing IMinfrastructureOper.delete_infrastructure """
         request_delete_mock.return_value = MockResponse({}, 200)
         anyheader = {"headerkey": "headervalue"}
         anyurl = 'any/url'
-        dv = IMinfrastructureOper.delete_infrastructure(anyheader, anyurl)
+        dv = probeim.IM.delete_infrastructure(anyheader, anyurl)
         assert dv.statuscode == 200
         assert dv.info == 'delete_infrastructure is OK'
 
