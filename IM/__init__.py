dc7f6976f2a13c42aa1e9112cfa6998272ad3570
micafer <micafer1@upv.es>
2019-03-29 08:02:25 +0100

    Change version to 1.8.3

--- IM/__init__.py
+++ IM/__init__.py
@@ -19,5 +19,5 @@ __all__ = ['auth', 'CloudInfo', 'config', 'ConfManager', 'db',
            'InfrastructureInfo', 'InfrastructureManager', 'recipe', 'request', 'REST', 'retry',
            'ServiceRequests', 'SSH', 'SSHRetry', 'timedcall', 'UnixHTTPAdapter',
            'VirtualMachine', 'VMRC', 'xmlobject']
-__version__ = '1.8.2'
+__version__ = '1.8.3'
 __author__ = 'Miguel Caballer'
