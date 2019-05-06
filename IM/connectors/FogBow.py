a2db2cccbe92e21d59a7f4da650bfa09252e2cf5
micafer <micafer1@upv.es>
2019-05-06 13:41:26 +0200

    Implements: #839

--- IM/connectors/FogBow.py
+++ IM/connectors/FogBow.py
@@ -250,9 +250,45 @@ class FogBowCloudConnector(CloudConnector):
 
                     net_info = self.post_and_get('/networks/', json.dumps(body), auth_data)
                     if net_info:
+                        self.log_debug("Network %s created successfully." % net_info['id'])
                         net.setValue("provider_id", net_info['id'])
+                        self.create_security_rules("networks", net, net_info['id'], auth_data)
                     else:
                         self.log_error("Error creating net %s." % net_name)
+
+    def create_security_rules(self, path, net, obj_id, auth_data):
+        """
+        Create security rules for a net
+        """
+        # First get the current opened ports
+        resp = self.create_request('GET', '/%s/%s/securityRules' % (path, obj_id), auth_data)
+        sec_groups = []
+        if resp.status_code == 200:
+            sec_groups = resp.json()
+
+        outports = net.getOutPorts()
+        if outports:
+            for outport in outports:
+                body = {"cidr": "0.0.0.0/0",
+                        "direction": "ingress",
+                        "etherType": "IPv4",
+                        "protocol": outport.get_protocol()
+                        }
+
+                if outport.is_range():
+                    body["portTo"] = outport.get_port_init()
+                    body["portFrom"] = outport.get_port_end()
+                else:
+                    body["portTo"] = outport.get_remote_port()
+                    body["portFrom"] = outport.get_remote_port()
+
+                if body not in sec_groups:
+                    headers = {'Content-Type': 'application/json'}
+                    resp = self.create_request('POST', '/%s/%s/securityRules' % (path, obj_id),
+                                               auth_data, headers, json.dumps(body))
+                    if resp.status_code not in [201, 200]:
+                        self.log_error("Error creating Security Rule in %s. %s. %s." % (resp.reason,
+                                                                                        path, resp.text))
 
     def launch(self, inf, radl, requested_radl, num_vm, auth_data):
         system = radl.systems[0]
@@ -471,42 +507,6 @@ class FogBowCloudConnector(CloudConnector):
             self.log_exception("Error getting public IP info")
         return res
 
-    def open_public_ip_ports(self, vm, ip_id, auth_data):
-        """
-        Open the requested ports in the public IP
-        """
-        # First get the current opened ports
-        resp = self.create_request('GET', '/publicIps/%s/securityRules' % ip_id, auth_data)
-        sec_groups = []
-        if resp.status_code == 200:
-            sec_groups = resp.json()
-
-        for net in vm.info.networks:
-            if net.isPublic() and vm.info.systems[0].getNumNetworkWithConnection(net.id) is not None:
-                outports = net.getOutPorts()
-                if outports:
-                    for outport in outports:
-                        body = {"cidr": "0.0.0.0/0",
-                                "direction": "ingress",
-                                "etherType": "IPv4",
-                                "protocol": outport.get_protocol()
-                                }
-
-                        if outport.is_range():
-                            body["portTo"] = outport.get_port_init()
-                            body["portFrom"] = outport.get_port_end()
-                        else:
-                            body["portTo"] = outport.get_remote_port()
-                            body["portFrom"] = outport.get_remote_port()
-
-                        if body not in sec_groups:
-                            headers = {'Content-Type': 'application/json'}
-                            resp = self.create_request('POST', '/publicIps/%s/securityRules' % ip_id, auth_data,
-                                                       headers, json.dumps(body))
-                            if resp.status_code not in [201, 200]:
-                                self.log_error("Error creating Public IP Security Rule. %s. %s." % (resp.reason,
-                                                                                                    resp.text))
-
     def add_elastic_ip(self, vm, public_ips, member, auth_data):
         """
         Get a public IP if needed.
@@ -526,7 +526,10 @@ class FogBowCloudConnector(CloudConnector):
             ip_info = self.post_and_get('/publicIps/', json.dumps(body), auth_data)
             if ip_info:
                 self.log_debug("IP obtained: %s." % ip_info['ip'])
-                self.open_public_ip_ports(vm, ip_info['id'], auth_data)
+                # Open ports for public IPs
+                for net in vm.info.networks:
+                    if net.isPublic() and vm.info.systems[0].getNumNetworkWithConnection(net.id) is not None:
+                        self.create_security_rules('publicIps', net, ip_info['id'], auth_data)
                 return ip_info['ip']
             else:
                 self.add_public_ip_count += 1
