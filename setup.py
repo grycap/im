8affc0096259e66cfd5b31ab8114376e79f67c87
micafer <micafer1@upv.es>
2019-09-02 11:49:18 +0200

    Update libcloud version

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(name="IM", version=version,
       description="IM is a tool to manage virtual infrastructures on Cloud deployments",
       platforms=["any"],
       install_requires=["ansible >= 2.0", "paramiko >= 1.14", "PyYAML", suds_pkg, sqlite_pkg, "cheroot",
-                        "boto >= 2.29", "apache-libcloud >= 2.4.0", "RADL >= 1.1.2", "bottle", "netaddr",
+                        "boto >= 2.29", "apache-libcloud >= 2.6.0", "RADL >= 1.1.2", "bottle", "netaddr",
                         "requests >= 2.19", "scp", "tosca-parser", 'defusedxml', 'urllib3>=1.23']
       )
