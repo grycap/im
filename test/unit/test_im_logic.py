3b903752e7f15502c65657f6d354fc0d23f076b9
micafer <micafer1@upv.es>
2019-09-02 16:30:50 +0200

    Implements #894

--- test/unit/test_im_logic.py
+++ test/unit/test_im_logic.py
@@ -35,6 +35,7 @@ Config.MAX_SIMULTANEOUS_LAUNCHES = 2
 
 from IM.VirtualMachine import VirtualMachine
 from IM.InfrastructureManager import InfrastructureManager as IM
+from IM.InfrastructureManager import DisabledFunctionException
 from IM.InfrastructureList import InfrastructureList
 from IM.auth import Authentication
 from radl.radl import RADL, system, deploy, Feature, SoftFeatures
@@ -1265,6 +1266,26 @@ configure step2 (
         time.sleep(6)
 
         IM.DestroyInfrastructure(infId, auth0)
+
+    def test_boot_modes(self):
+        """Test boot modes"""
+        auth0 = self.getAuth([0], [], [("Dummy", 0), ("Dummy", 1)])
+        infId = IM.CreateInfrastructure('', auth0)
+
+        Config.BOOT_MODE = 1
+        with self.assertRaises(DisabledFunctionException):
+            IM.DestroyInfrastructure(infId, auth0)
+        with self.assertRaises(DisabledFunctionException):
+            IM.CreateInfrastructure('', auth0)
+        IM.GetInfrastructureList(auth0)
+
+        Config.BOOT_MODE = 2
+        with self.assertRaises(DisabledFunctionException):
+            IM.CreateInfrastructure('', auth0)
+        IM.DestroyInfrastructure(infId, auth0)
+        IM.GetInfrastructureList(auth0)
+
+        Config.BOOT_MODE = 0
 
 
 if __name__ == "__main__":
