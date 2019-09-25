443c499af7a984612e38971182d3954c19bdc7ab
micafer <micafer1@upv.es>
2019-09-25 14:07:56 +0200

    Update ssh2 package version

--- contextualization/ansible_install.sh
+++ contextualization/ansible_install.sh
@@ -67,8 +67,8 @@ else
             apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
             apt-get update
             apt-get -y install wget ansible
-            wget https://github.com/ParallelSSH/ssh2-python/releases/download/0.15.0.post9/python-ssh2-python_0.15.0.post9-debian8_amd64.deb
-            dpkg -i python-ssh2-python_0.15.0.post9-debian8_amd64.deb
+            wget http://ftpgrycap.i3m.upv.es/ssh2-python/python-ssh2-python-0.18.0_1_ge5fdd3e-1.debian8.x86_64.rpm
+            dpkg -i python-ssh2-python-0.18.0_1_ge5fdd3e-1.debian8.x86_64.rpm
             apt install -f -y
             ;;
         ubuntu)
@@ -76,23 +76,23 @@ else
             apt-add-repository -y ppa:ansible/ansible
             apt-get update
             apt-get -y install wget ansible
-            wget https://github.com/ParallelSSH/ssh2-python/releases/download/0.15.0.post9/python-ssh2-python_0.15.0.post9-ubuntu$(distribution_major_version)_amd64.deb
-            dpkg -i python-ssh2-python_0.15.0.post9-ubuntu$(distribution_major_version)_amd64.deb
+            wget http://ftpgrycap.i3m.upv.es/ssh2-python/python-ssh2-python-0.18.0_1_ge5fdd3e-1.ubuntu$(distribution_major_version).x86_64.rpm
+            dpkg -i python-ssh2-python-0.18.0_1_ge5fdd3e-1.ubuntu$(distribution_major_version).x86_64.rpm
             apt install -f -y
             ;;
         rhel)
             yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-$(distribution_major_version).noarch.rpm
             yum install -y wget ansible
-            yum install -y https://github.com/ParallelSSH/ssh2-python/releases/download/0.15.0.post9/python-ssh2-python-0.15.0.post9-1.el7.x86_64.rpm
+            yum install -y http://ftpgrycap.i3m.upv.es/ssh2-python/python-ssh2-python-0.18.0_1_ge5fdd3e-1.el7.x86_64.rpm
             ;;
         centos)
             yum install -y epel-release wget
             yum install -y ansible
-            yum install -y https://github.com/ParallelSSH/ssh2-python/releases/download/0.15.0.post9/python-ssh2-python-0.15.0.post9-1.el7.x86_64.rpm
+            yum install -y http://ftpgrycap.i3m.upv.es/ssh2-python/python-ssh2-python-0.18.0_1_ge5fdd3e-1.el7.x86_64.rpm
             ;;
         fedora)
             yum install -y wget ansible python2-rpm yum
-            yum install -y https://github.com/ParallelSSH/ssh2-python/releases/download/0.15.0.post9/python-ssh2-python-0.15.0.post9-1.fc$(distribution_major_version).x86_64.rpm
+            yum install -y http://ftpgrycap.i3m.upv.es/ssh2-python/python-ssh2-python-0.18.0_1_ge5fdd3e-1.fc$(distribution_major_version).x86_64.rpm
             ;;
     	*)
             echo "Unsupported distribution: $DISTRO"
