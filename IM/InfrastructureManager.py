4fac0a89c93e293a1f324a2d3abe87cc6adcc95d
micafer <micafer1@upv.es>
2019-10-30 12:22:59 +0100

    Style changes

--- IM/InfrastructureManager.py
+++ IM/InfrastructureManager.py
@@ -181,8 +181,9 @@ class InfrastructureManager:
             requested_radl.systems = [radl.get_system_by_name(deploy.id)]
             if not concrete_system:
                 InfrastructureManager.logger.error("Inf ID: " + str(sel_inf.id) +
-                    ". Error, no concrete system to deploy: " + deploy.id + " in cloud: " +
-                    cloud_id + ". Check if a correct image is being used")
+                                                   ". Error, no concrete system to deploy: " +
+                                                   deploy.id + " in cloud: " + cloud_id +
+                                                   ". Check if a correct image is being used")
                 for _ in range(deploy.vm_number):
                     launched_vms.append((False, "Error, no concrete system to deploy: " + deploy.id +
                                          " in cloud: " + cloud_id + ". Check if a correct image is being used"))
@@ -210,13 +211,13 @@ class InfrastructureManager:
 
             for success, launched_vm in launched_vms:
                 if success:
-                    InfrastructureManager.logger.debug("Inf ID: %s. VM successfully launched: %s" % (sel_inf.id, 
+                    InfrastructureManager.logger.debug("Inf ID: %s. VM successfully launched: %s" % (sel_inf.id,
                                                                                                      launched_vm.id))
                     deployed_vm.setdefault(deploy, []).append(launched_vm)
                     deploy.cloud_id = cloud_id
                 else:
-                    InfrastructureManager.logger.error("Inf ID: %s. Error launching some of the VMs: %s" % (sel_inf.id,
-                                                                                                            launched_vm))
+                    InfrastructureManager.logger.error("Inf ID: %s. Error launching some of the "
+                                                       "VMs: %s" % (sel_inf.id, launched_vm))
                     vm = VirtualMachine(sel_inf, None, cloud.cloud, launch_radl, requested_radl)
                     vm.state = VirtualMachine.FAILED
                     vm.info.systems[0].setValue('state', VirtualMachine.FAILED)
