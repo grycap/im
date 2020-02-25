91224f14e8bdd0c18f93ca277dc2c78a2a037a3b
micafer <micafer1@upv.es>
2020-02-25 10:35:25 +0100

    Set version num 1.9.2

--- IM/__init__.py
+++ IM/__init__.py
@@ -19,7 +19,7 @@ __all__ = ['auth', 'CloudInfo', 'config', 'ConfManager', 'db',
            'InfrastructureInfo', 'InfrastructureManager', 'recipe', 'request', 'REST', 'retry',
            'ServiceRequests', 'SSH', 'SSHRetry', 'timedcall', 'UnixHTTPAdapter',
            'VirtualMachine', 'VMRC', 'xmlobject']
-__version__ = '1.9.1'
+__version__ = '1.9.2'
 __author__ = 'Miguel Caballer'
 
 
