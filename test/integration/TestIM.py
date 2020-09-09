04d96d475a9dc4056e6ed5ad7fdc32532f0c630f
micafer <micafer1@upv.es>
2020-09-09 08:43:35 +0200

    Revert test

--- test/integration/TestIM.py
+++ test/integration/TestIM.py
@@ -689,7 +689,7 @@ echo "Hello World" >> /tmp/data.txt
         self.__class__.inf_id = [inf_id]
 
         all_configured = self.wait_inf_state(
-            inf_id, VirtualMachine.CONFIGURED, 1200)
+            inf_id, VirtualMachine.CONFIGURED, 900)
         self.assertTrue(
             all_configured, msg="ERROR waiting the infrastructure to be configured (timeout).")
 
