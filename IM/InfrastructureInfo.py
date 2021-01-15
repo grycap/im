567526a1f4cf2456f8db1898f1a1f16148adba46
micafer <micafer1@upv.es>
2021-01-15 09:05:53 +0100

    Add creation_date to inf

--- IM/InfrastructureInfo.py
+++ IM/InfrastructureInfo.py
@@ -149,6 +149,7 @@ class InfrastructureInfo:
     @staticmethod
     def deserialize(str_data):
         newinf = InfrastructureInfo()
+        newinf.creation_date = None
         dic = json.loads(str_data)
         vm_list = dic['vm_list']
         vm_master_id = dic['vm_master']
