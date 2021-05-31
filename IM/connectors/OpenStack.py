grycap/im

commit b2d36c73c20f3d088aa18f63342463ccfd6bd4a4

Fix #1185

IM/connectors/OpenStack.py
@@ -994,7 +994,7 @@ class OpenStackCloudConnector(LibCloudCloudConnector):
 
         boot_disk = {
             'boot_index': 0,
-            'device_name': 'vda',
+#            'device_name': 'vda',
             'source_type': "image",
             'delete_on_termination': False,
             'uuid': image.id

test/unit/connectors/OpenStack.py
@@ -252,8 +252,7 @@ class TestOSTConnector(TestCloudConnectorBase):
             {'source_type': 'image',
              'uuid': 'imageid',
              'boot_index': 0,
-             'delete_on_termination': False,
-             'device_name': 'vda'},
+             'delete_on_termination': False},
             {'guest_format': 'ext3',
              'boot_index': 1,
              'volume_size': 1,

commit 098de7e9f19f7c18fbf61c96f4c8ee538803447a

Fix error setting boot disk size caused by b2d36c73c20f3d088aa18f63342463ccfd6bd4a4

IM/connectors/OpenStack.py
@@ -1005,7 +1005,6 @@ class OpenStackCloudConnector(LibCloudCloudConnector):
             boot_disk['destination_type'] = 'volume'
             boot_disk['volume_size'] = system.getFeature("disk.0.size").getValue('G')
             boot_disk['delete_on_termination'] = True
-            del boot_disk['device_name']
 
         res = [boot_disk]
 