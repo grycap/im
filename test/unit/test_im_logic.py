697822e3e4143e59162f8e083e2eb1a0b63e1852
Miguel Caballer <micafer1@upv.es>
2021-10-13 08:33:40 +0200

    increase timeout

--- test/unit/test_im_logic.py
+++ test/unit/test_im_logic.py
@@ -530,7 +530,7 @@ class TestIM(unittest.TestCase):
         Config.MAX_SIMULTANEOUS_LAUNCHES = 1
         vms = IM.AddResource(infId, str(radl), auth0)
         delay = int(time.time()) - before
-        self.assertLess(delay, 19)
+        self.assertLess(delay, 21)
         self.assertGreater(delay, 14)
 
         self.assertEqual(vms, [0, 1, 2, 3, 4, 5])
