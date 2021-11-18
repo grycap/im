46752377533d5e94e244cb315e1807c3f0356a59
micafer <micafer1@upv.es>
2021-11-18 08:31:21 +0100

    Add print

--- test/unit/Tosca.py
+++ test/unit/Tosca.py
@@ -49,6 +49,7 @@ class TestTosca(unittest.TestCase):
         tosca_data = read_file_as_string('../files/tosca_long.yml')
         tosca = Tosca(tosca_data)
         _, radl = tosca.to_radl()
+        print(radl)
         radl = parse_radl(str(radl))
         net = radl.get_network_by_id('public_net')
         net1 = radl.get_network_by_id('public_net_1')
