94a094628f2470dba79c6b5ee74a0cb4a67d0bbe
micafer <micafer1@upv.es>
2022-03-24 11:09:35 +0100

    Use PRIVATE_NET_MASKS in Azure conn

--- test/unit/connectors/Azure.py
+++ test/unit/connectors/Azure.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.command.config import config
 import sys
 import unittest
 
