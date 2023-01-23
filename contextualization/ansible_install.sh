94b1a35dea163dc3d9b42543f42f68490f6fbf45
Miguel Caballer <micafer1@upv.es>
2023-01-23 11:37:03 +0100

    Fix #1457

--- contextualization/ansible_install.sh
+++ contextualization/ansible_install.sh
@@ -71,7 +71,7 @@ else
 
     pip3 install "pip>=20.0"
     pip3 install -U "setuptools<66.0"
-    pip3 install pyOpenSSL pyyaml jmespath scp paramiko>=2.9.5 --prefer-binary
+    pip3 install pyOpenSSL>20.0,<22.1.0 cryptography>37.0.0,<39.0.0 pyyaml jmespath scp paramiko>=2.9.5 --prefer-binary
     pip3 install ansible==$ANSIBLE_VERSION --prefer-binary
 fi
 
