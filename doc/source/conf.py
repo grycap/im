50d31ca88ba0519976d007161c614dbf7ebfa654
micafer <micafer1@upv.es>
2023-05-09 09:20:36 +0200

    improve docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,10 +9,7 @@ release = '1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
 
