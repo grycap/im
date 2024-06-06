9ff5074e89746417dba9bc8aa63f9f747882a286
Miguel Caballer <micafer1@upv.es>
2024-06-06 08:21:33 +0200

    Remove obsolete packages generators

--- packages/generate_rpm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum -y install rpm-build python-setuptools
-echo "%_unpackaged_files_terminate_build 0" > ~/.rpmmacros
-python setup.py bdist_rpm --release="$1" --requires="which, MySQL-python, python-sqlite3dbm, RADL, ansible, python-paramiko, PyYAML, python-suds, python-boto >= 2.29, python-libcloud, python-bottle, python-netaddr, python-scp, python-cherrypy, python-requests, python-xmltodict, tosca-parser"
-mkdir dist_pkg
-cp dist/*.noarch.rpm dist_pkg
