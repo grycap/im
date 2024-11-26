b596d223fd8d68c00e94cf2287af20abd23ddaac
Miguel Caballer <micafer1@upv.es>
2024-11-26 16:30:51 +0100

    Fix ctxt dist path

--- IM/ConfManager.py
+++ IM/ConfManager.py
@@ -391,15 +391,16 @@ class ConfManager(LoggerMixin, threading.Thread):
                     if len(self.inf.get_vm_list()) > Config.VM_NUM_USE_CTXT_DIST:
                         self.log_info("Using ctxt_agent_dist")
                         ctxt_agent_command = "/ctxt_agent_dist.py "
+                        python_path = "python3 "
                     else:
                         self.log_info("Using ctxt_agent")
                         ctxt_agent_command = "/ctxt_agent.py "
+                        python_path = CtxtAgentBase.VENV_DIR + "/bin/python3 "
                     vault_export = ""
                     vault_password = vm.info.systems[0].getValue("vault.password")
                     if vault_password:
                         vault_export = "export VAULT_PASS='%s' && " % vault_password
-                    (pid, _, _) = ssh.execute("nohup sh -c \"" + vault_export + CtxtAgentBase.VENV_DIR +
-                                              "/bin/python3 " + Config.REMOTE_CONF_DIR +
+                    (pid, _, _) = ssh.execute("nohup sh -c \"" + vault_export + python_path + Config.REMOTE_CONF_DIR +
                                               "/" + str(self.inf.id) + "/" + ctxt_agent_command +
                                               Config.REMOTE_CONF_DIR + "/" + str(self.inf.id) + "/" +
                                               "/general_info.cfg " + remote_dir + "/" + os.path.basename(conf_file) +
