e14fb14061a60668c39248441957586c9c6dbc22
Miguel Caballer <micafer1@upv.es>
2025-10-27 10:30:41 +0100

    Merge with flast-restx

--- IM/awm/__init__.py
+++ IM/awm/__init__.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+import json
+import os
+import yaml
+from flask import Blueprint, request
+from flask_restx import Api
 from IM.awm.routers.allocations import allocations_bp
 from IM.awm.routers.deployments import deployments_bp
 from IM.awm.routers.service import service_bp
@@ -7,6 +11,23 @@ from IM.awm.routers.users import users_bp
 
 
 awm_bp = Blueprint("awm", __name__)
+awm_spec = None
+
+
+@awm_bp.route('/swagger.json')
+def awm_swagger():
+    awm_path = os.path.dirname(os.path.abspath(__file__))
+    abs_file_path = os.path.join(awm_path, 'awm-api.yaml')
+    global awm_spec
+    if awm_spec is None:
+        with open(abs_file_path, 'r', encoding='utf-8') as f:
+            awm_spec = yaml.safe_load(f)
+            awm_spec['servers'] = [{
+                'url': f'{request.url_root}',
+                'description': 'Local server'
+            }]
+    return yaml.safe_dump(awm_spec), 200, {'Content-Type': 'application/x-yaml'}
+
 
 # registrar los blueprints hijos dentro del principal
 awm_bp.register_blueprint(allocations_bp, url_prefix="/allocations")
@@ -14,3 +35,5 @@ awm_bp.register_blueprint(deployments_bp, url_prefix="/deployments")
 awm_bp.register_blueprint(tools_bp, url_prefix="/tools")
 awm_bp.register_blueprint(service_bp, url_prefix="/version")
 awm_bp.register_blueprint(users_bp, url_prefix="/user")
+
+api = Api(awm_bp, title="AWM API")
