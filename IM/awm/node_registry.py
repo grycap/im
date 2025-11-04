05370278d21b145b551f2234ac382455db66e432
Miguel Caballer <micafer1@upv.es>
2025-11-04 12:15:22 +0100

    Minor change

--- IM/awm/node_registry.py
+++ IM/awm/node_registry.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, HttpUrl
+from typing import List
 
 
 class EOSCNode(BaseModel):
@@ -11,12 +12,12 @@ class EOSCNode(BaseModel):
 class EOSCNodeRegistry():
     """Class to interact with the central EOSC Node Registry"""
 
-    def list_nodes(self): 
+    def list_nodes(self) -> List[EOSCNode]:
         """Return the list of available nodes"""
         # @TODO(list): Complete
         return []
 
-    def get_node_by_id(self, node_id):
+    def get_node_by_id(self, node_id: str) -> EOSCNode:
         """Retun the node with ID `node_id`"""
         # @TODO(get): Complete
         return None
