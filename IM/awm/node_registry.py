b55cf604bf4321e9dafab5c858d58d5bdf2ab163
Miguel Caballer <micafer1@upv.es>
2025-11-05 12:08:08 +0100

    Minor changes

--- IM/awm/node_registry.py
+++ IM/awm/node_registry.py
@@ -1,8 +1,12 @@
+import requests
+import logging
 from pydantic import BaseModel, HttpUrl
 from typing import List, Tuple
 from IM.awm.models.tool import ToolInfo
 from IM.awm.models.page import PageOfTools
-import requests
+
+
+logger = logging.getLogger(__name__)
 
 
 class EOSCNode(BaseModel):
@@ -12,18 +16,21 @@ class EOSCNode(BaseModel):
     awmAPI: HttpUrl
 
     def list_tools(self, from_: int, limit: int, count: int, token: str) -> Tuple[int, List[ToolInfo]]:
+        """Return the list of tools of this node"""
         init = max(0, from_ - count)
         elems = limit - (count - from_)
         url = f"{self.awmAPI}tools?from0&limit={elems}"
         try:
             headers = {"Authorization": f"Bearer {token}"}
             response = requests.get(url, headers=headers, timeout=30)
+            response.raise_for_status()
             if response.status_code == 200:
                 page = PageOfTools.model_validate(response.json())
                 tools = page.elements[init:] if len(page.elements) > init else []
                 return page.count, tools
         except Exception:
-            return 0, []
+            logger.exception("Error getting tools from node: %s", self.nodeId)
+        return 0, []
 
 
 class EOSCNodeRegistry():
