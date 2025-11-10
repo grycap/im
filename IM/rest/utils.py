cb10f8ebca8df035bd8f01696bca95b1dbbf48cf
Miguel Caballer <micafer1@upv.es>
2025-11-10 13:48:51 +0100

    Add license header

--- IM/rest/utils.py
+++ IM/rest/utils.py
@@ -1,5 +1,5 @@
 # IM - Infrastructure Manager
-# Copyright (C) 2011 - GRyCAP - Universitat Politecnica de Valencia
+# Copyright (C) 2025 - GRyCAP - Universitat Politecnica de Valencia
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
